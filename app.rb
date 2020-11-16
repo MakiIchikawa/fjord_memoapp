e66d58a0f24296b7642b5ef256af465139f068ca
MakiIchikawa <punkfolkfolk@gmail.com>
2020-11-16 10:25:08 +0900

    add the functions of show_memo and edit_memo

--- app.rb
+++ app.rb
@@ -10,10 +10,10 @@ end
 get '/top' do
   @title = 'top|memoapp'
   memo = []
-  Dir.each_child('./memo') do |f|
+  Dir.children('./memo').sort.each do |f|
     file = File.open("./memo/#{f}", 'r')
-    content = file.readline.split(/,/)
-    memo.push([File.basename(f, '.*'), content[0]])
+    file_content = file.readline.split(/,/)
+    memo.push([File.basename(f, '.*'), file_content[0]])
     file.close
   end
   @memo = memo
@@ -34,4 +34,36 @@ post '/memo' do
     f.write("#{params[:title]},#{params[:content]}")
   end
   p '保存しました'
+end
+
+get '/memo/:id' do
+  @memo_id = params[:id]
+  file = File.open("./memo/#{params[:id]}.txt", 'r')
+  file_content = file.readline.split(/,/)
+  @memo_title = file_content[0]
+  @memo_content = file_content[1]
+  file.close
+  erb :show
+end
+
+delete '/memo/:id' do
+  File.delete("./memo/#{params[:id]}.txt")
+  p '削除しました'
+end
+
+get '/edit/:id' do
+  @memo_id = params[:id]
+  file = File.open("./memo/#{params[:id]}.txt", 'r')
+  file_content = file.readline.split(/,/)
+  @memo_title = file_content[0]
+  @memo_content = file_content[1]
+  file.close
+  erb :edit
+end
+
+patch '/memo/:id' do
+  file = File.open("./memo/#{params[:id]}.txt", 'w')
+  file.write("#{params[:title]},#{params[:content]}")
+  file.close
+  p '変更しました。'
 end
